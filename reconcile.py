f9041552abff285a74e6d57d7fcdb79794bb450c
nruest <ruestn@gmail.com>
2020-10-15 22:13:02 -0400

    Add Python formatting GitHub Workflow.
    - Apply isort and black

--- reconcile.py
+++ reconcile.py
@@ -5,82 +5,73 @@ GeoNames API documentation: http://www.geonames.org/export/web-services.html
 
 This code is adapted from the wonderful Ted Lawless' work at https://github.com/lawlesst/fast-reconcile
 """
-from flask import Flask
-from flask import request
-from flask import jsonify
 import json
-from operator import itemgetter
 import urllib
-from fuzzywuzzy import fuzz
-import requests
+from operator import itemgetter
 from sys import version_info
 
+import requests
+from flask import Flask, jsonify, request
+from fuzzywuzzy import fuzz
+
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object("config")
 
-geonames_username = app.config['GEONAMES_USERNAME']
+geonames_username = app.config["GEONAMES_USERNAME"]
 
-#If it's installed, use the requests_cache library to
-#cache calls to the GeoNames API.
+# If it's installed, use the requests_cache library to
+# cache calls to the GeoNames API.
 try:
     import requests_cache
-    requests_cache.install_cache('geonames_cache')
+
+    requests_cache.install_cache("geonames_cache")
 except ImportError:
     app.logger.debug("No request cache found.")
     pass
 
-#See if Python 3 for unicode/str use decisions
+# See if Python 3 for unicode/str use decisions
 PY3 = version_info > (3,)
 
-#Help text processing
-import text
 import lc_parse
+# Help text processing
+import text
 
-#Create base URLs/URIs
-api_base_url = 'http://api.geonames.org/searchJSON?username=' + geonames_username + '&isNameRequired=yes&'
-geonames_uri_base = 'http://sws.geonames.org/{0}'
+# Create base URLs/URIs
+api_base_url = (
+    "http://api.geonames.org/searchJSON?username="
+    + geonames_username
+    + "&isNameRequired=yes&"
+)
+geonames_uri_base = "http://sws.geonames.org/{0}"
 
-#Map the GeoNames query indexes to service types
-default_query = {
-    "id": "/geonames/all",
-    "name": "All GeoNames terms",
-    "index": "q"
-}
+# Map the GeoNames query indexes to service types
+default_query = {"id": "/geonames/all", "name": "All GeoNames terms", "index": "q"}
 
 refine_to_geonames = [
-    {
-        "id": "/geonames/name",
-        "name": "Place Name",
-        "index": "name"
-    },
+    {"id": "/geonames/name", "name": "Place Name", "index": "name"},
     {
         "id": "/geonames/name_startsWith",
         "name": "Place Name Starts With",
-        "index": "name_startsWith"
+        "index": "name_startsWith",
     },
-    {
-        "id": "/geonames/name_equals",
-        "name": "Exact Place Name",
-        "index": "name_equals"
-    }
+    {"id": "/geonames/name_equals", "name": "Exact Place Name", "index": "name_equals"},
 ]
 refine_to_geonames.append(default_query)
 
 
-#Make a copy of the GeoNames mappings.
-query_types = [{'id': item['id'], 'name': item['name']} for item in refine_to_geonames]
+# Make a copy of the GeoNames mappings.
+query_types = [{"id": item["id"], "name": item["name"]} for item in refine_to_geonames]
 
 # Basic service metadata. There are a number of other documented options
 # but this is all we need for a simple service.
 metadata = {
     "name": "GeoNames Reconciliation Service",
-    "identifierSpace" : "http://localhost/identifier",
-    "schemaSpace" : "http://localhost/schema",
+    "identifierSpace": "http://localhost/identifier",
+    "schemaSpace": "http://localhost/schema",
     "defaultTypes": query_types,
-    "view": {
-        "url": "{{id}}"
-    }
+    "view": {"url": "{{id}}"},
 }
+
 
 def make_uri(geonames_id):
     """
@@ -95,7 +86,7 @@ def jsonpify(obj):
     Helper to support JSONP
     """
     try:
-        callback = request.args['callback']
+        callback = request.args["callback"]
         response = app.make_response("%s(%s)" % (callback, json.dumps(obj)))
         response.mimetype = "text/javascript"
         return response
@@ -103,7 +94,7 @@ def jsonpify(obj):
         return jsonify(obj)
 
 
-def search(raw_query, query_type='/geonames/all'):
+def search(raw_query, query_type="/geonames/all"):
     """
     Hit the GeoNames API for names.
     """
@@ -111,36 +102,36 @@ def search(raw_query, query_type='/geonames/all'):
     unique_geonames_ids = []
     mid_query = lc_parse.lc2geonames(raw_query, PY3)
     query = text.normalize(mid_query, PY3).strip()
-    query_type_meta = [i for i in refine_to_geonames if i['id'] == query_type]
+    query_type_meta = [i for i in refine_to_geonames if i["id"] == query_type]
     if query_type_meta == []:
         query_type_meta = default_query
-    query_index = query_type_meta[0]['index']
+    query_index = query_type_meta[0]["index"]
     try:
         if PY3:
-            url = api_base_url + query_index  + '=' + urllib.parse.quote(query)
+            url = api_base_url + query_index + "=" + urllib.parse.quote(query)
         else:
-            url = api_base_url + query_index  + '=' + urllib.quote(query)
+            url = api_base_url + query_index + "=" + urllib.quote(query)
         app.logger.debug("GeoNames API url is " + url)
         resp = requests.get(url)
         results = resp.json()
     except getopt.GetoptError as e:
         app.logger.warning(e)
         return out
-    for position, item in enumerate(results['geonames']):
+    for position, item in enumerate(results["geonames"]):
         match = False
-        name = item.get('name')
-        alternate = item.get('toponymName')
-        if (len(alternate) > 0):
+        name = item.get("name")
+        alternate = item.get("toponymName")
+        if len(alternate) > 0:
             alt = alternate[0]
         else:
-            alt = ''
-        geonames_id = item.get('geonameId')
+            alt = ""
+        geonames_id = item.get("geonameId")
         geonames_uri = make_uri(geonames_id)
-        lat = item.get('lat')
-        lng = item.get('lng')
-        #Way to cheat + get name + coordinates into results:
-        name_coords = name + ' | ' + lat + ', ' + lng
-        #Avoid returning duplicates:
+        lat = item.get("lat")
+        lng = item.get("lng")
+        # Way to cheat + get name + coordinates into results:
+        name_coords = name + " | " + lat + ", " + lng
+        # Avoid returning duplicates:
         if geonames_id in unique_geonames_ids:
             continue
         else:
@@ -157,39 +148,41 @@ def search(raw_query, query_type='/geonames/all'):
             "name": name_coords,
             "score": score,
             "match": match,
-            "type": query_type_meta
+            "type": query_type_meta,
         }
         out.append(resource)
-    #Sort this list by score
-    sorted_out = sorted(out, key=itemgetter('score'), reverse=True)
-    #Refine only will handle top three matches.
+    # Sort this list by score
+    sorted_out = sorted(out, key=itemgetter("score"), reverse=True)
+    # Refine only will handle top three matches.
     return sorted_out[:3]
 
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def reconcile():
     # If a 'queries' parameter is supplied then it is a dictionary
     # of (key, query) pairs representing a batch of queries. We
     # should return a dictionary of (key, results) pairs.
-    queries = request.form.get('queries')
+    queries = request.form.get("queries")
     if queries:
         queries = json.loads(queries)
         results = {}
         for (key, query) in queries.items():
-            qtype = query.get('type')
+            qtype = query.get("type")
             if qtype is None:
                 return jsonpify(metadata)
-            data = search(query['query'], query_type=qtype)
+            data = search(query["query"], query_type=qtype)
             results[key] = {"result": data}
         return jsonpify(results)
     # If neither a 'query' nor 'queries' parameter is supplied then
     # we should return the service metadata.
     return jsonpify(metadata)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from optparse import OptionParser
+
     oparser = OptionParser()
-    oparser.add_option('-d', '--debug', action='store_true', default=False)
+    oparser.add_option("-d", "--debug", action="store_true", default=False)
     opts, args = oparser.parse_args()
     app.debug = opts.debug
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
--- text.py
+++ text.py
@@ -3,59 +3,63 @@ Taken from the Helmut project.
 https://github.com/okfn/helmut/blob/master/helmut/text.py
 """
 
-from unicodedata import normalize as ucnorm, category
+from unicodedata import category
+from unicodedata import normalize as ucnorm
+
 
 def normalize(text, PY3):
-    """ Simplify a piece of text to generate a more canonical
+    """Simplify a piece of text to generate a more canonical
     representation. This involves lowercasing, stripping trailing
     spaces, removing symbols, diacritical marks (umlauts) and
     converting all newlines etc. to single spaces.
     """
     if PY3:
         if not isinstance(text, str):
-            str(text, 'utf-8')
+            str(text, "utf-8")
     else:
         if not isinstance(text, unicode):
             text = unicode(text)
     text = text.lower()
-    decomposed = ucnorm('NFKD', text)
+    decomposed = ucnorm("NFKD", text)
     filtered = []
     for char in decomposed:
         cat = category(char)
-        if cat.startswith('C'):
-            filtered.append(' ')
-        elif cat.startswith('M'):
+        if cat.startswith("C"):
+            filtered.append(" ")
+        elif cat.startswith("M"):
             # marks, such as umlauts
             continue
-        elif cat.startswith('Z'):
+        elif cat.startswith("Z"):
             # newlines, non-breaking etc.
-            filtered.append(' ')
-        elif cat.startswith('S'):
+            filtered.append(" ")
+        elif cat.startswith("S"):
             # symbols, such as currency
             continue
         else:
             filtered.append(char)
-    text = u''.join(filtered)
-    while '  ' in text:
-        text = text.replace('  ', ' ')
-    #remove hyphens
-    text = text.replace('-', ' ')
+    text = u"".join(filtered)
+    while "  " in text:
+        text = text.replace("  ", " ")
+    # remove hyphens
+    text = text.replace("-", " ")
     text = text.strip()
-    return ucnorm('NFKC', text)
+    return ucnorm("NFKC", text)
+
 
 def url_slug(text, PY3):
     text = normalize(text)
-    text = text.replace(' ', '-')
-    text = text.replace('.', '_')
+    text = text.replace(" ", "-")
+    text = text.replace(".", "_")
     return text
 
-def tokenize(text, splits='COPZ'):
+
+def tokenize(text, splits="COPZ"):
     token = []
     if PY3:
-        for c in str(text, 'utf-8'):
+        for c in str(text, "utf-8"):
             if category(c)[0] in splits:
                 if len(token):
-                    yield u''.join(token)
+                    yield u"".join(token)
                 token = []
             else:
                 token.append(c)
@@ -63,9 +67,9 @@ def tokenize(text, splits='COPZ'):
         for c in unicode(text):
             if category(c)[0] in splits:
                 if len(token):
-                    yield u''.join(token)
+                    yield u"".join(token)
                 token = []
             else:
                 token.append(c)
     if len(token):
-        yield u''.join(token)
+        yield u"".join(token)
